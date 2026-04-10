3fb76edce172e5f87395c6f20105607d855ad66a
haroon haneez <himazhaneez2005@gmail.com>
2026-04-10 18:55:21 +0530

    feat: add prisma seed script for admin user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,30 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
-  const adminEmail = "headadmin@insight.edu";
-  
+  const adminEmail = 'admin@example.com'
+  const adminPassword = 'adminpassword123' // Change this to something secure!
+
   const admin = await prisma.user.upsert({
     where: { email: adminEmail },
     update: {},
     create: {
       email: adminEmail,
-      password: "adminpassword123",
-      role: "SUPER_ADMIN",
-      schoolName: "Insight Head Office",
+      password: adminPassword,
+      role: 'SUPER_ADMIN',
     },
-  });
+  })
 
-  console.log({ admin });
+  console.log({ admin })
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
